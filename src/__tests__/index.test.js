510d00928276d8b57c1eaf56d0d390f0f3a7e38d
wangyongxin <676425826@qq.com>
2023-08-02 00:15:29 +0800

    添加test文件

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,8 @@
+import { name, py, pinxie } from '../name';
+
+it('=> name.length', () => expect(name.length).toBe(568));
+it('=> pinxie.length', () => expect(pinxie.length).toBe(568));
+it('=> py.length', () => expect(py.length).toBe(568));
+it('=> name[3]', () => expect(name[3]).toBe('李'));
+it('=> py[2]', () => expect(py[2]).toBe('sūn'));
+it('=> pinxie[1]', () => expect(pinxie[1]).toBe('qian'));
